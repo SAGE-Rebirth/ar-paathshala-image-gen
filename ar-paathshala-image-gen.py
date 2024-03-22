4bdeac50992aaa8f1ad7498a12d91f90533d6502
SAGE-Rebirth <abhinavs1711@gmail.com>
2024-03-22 23:36:39 +0530

    Image gen script uploaded

---
+++ ar-paathshala-image-gen.py
@@ -0,0 +1,21 @@
+from diffusers import StableDiffusionPipeline
+import torch
+import gradio as gr
+
+def ArPaathshsalaImageApp(prompt):
+  model_id = "prompthero/openjourney"
+  pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+  pipe = pipe.to("cuda")
+
+  prompt = prompt
+  image = pipe(prompt).images[0]
+  return image
+
+
+app = gr.Interface(ArPaathshsalaImageApp,
+                   inputs = "text",
+                   outputs = "image",
+                   theme = gr.themes.Soft(),
+                   title = "AR Paathshala Image Generator")
+
+#app.launch()
---
+++ readme.md
@@ -0,0 +1,2 @@
+To run this first install all the packages - 
+pip install -q diffusers transformers accelerate gradio
